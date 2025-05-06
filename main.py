4378645903957fb233dfa3f6b177ab4196cb4ae6
28bastolau <28bastolau@bwschools.net>
2025-05-06 12:33:33 -0400

    commit

---
+++ main.py
@@ -0,0 +1,137 @@
+import turtle
+screen=turtle.Screen()
+screen.screensize(500,500)
+screen.bgcolor("tan")
+t = turtle.Turtle()
+
+t.penup()
+t.goto(0,0)
+t.pendown()
+t.write("Hello",font=("Arial",30,"bold"),align="center")
+round = input("press Enter to Continue: ")
+t.clear()
+
+t.penup()
+t.goto(0,0)
+t.pendown()
+screen.addshape("messi.gif")
+t.shape('messi.gif')
+t.stamp()
+t.shape("classic")
+t.penup()
+t.goto(70,50)
+t.pendown()
+t.write("I like messi he's the goat",font=("Arial",25,"bold"),align="center")
+round = input("press Enter to Continue: ")
+t.clear()
+screen.bgcolor('tan')
+
+
+t.penup()
+t.goto(0,0)
+t.pendown()
+screen.bgcolor('aqua')
+screen.addshape('pizza.gif')
+t.shape('pizza.gif')
+t.stamp()
+t.shape('pizza.gif')
+
+
+
+round = input("press Enter to Continue: ")
+t.clear()
+screen.bgcolor('yellow')
+t.penup()
+t.goto(0,0)
+t.pendown()
+screen.addshape('tik.gif')
+t.shape('tik.gif')
+t.stamp()
+t.shape('tik.gif')
+
+
+
+
+
+round = input("press Enter to Continue: ")
+t.clear()
+screen.bgcolor('grey')
+t.penup()
+t.goto(0,0)
+t.pendown()
+screen.addshape('sleep.gif')
+t.shape('sleep.gif')
+t.stamp()
+t.shape('sleep.gif')
+
+
+
+
+
+round = input("press Enter to Continue: ")
+t.clear()
+screen.bgcolor('purple')
+t.penup()
+t.goto(0,0)
+t.pendown()
+screen.addshape('soccer.gif')
+t.shape('soccer.gif')
+t.stamp()
+t.shape('soccer.gif')
+
+
+
+
+round = input("press Enter to Continue: ")
+t.clear()
+screen.bgcolor('tan')
+t.penup()
+t.goto(0,0)
+t.pendown()
+screen.addshape('pizza.gif')
+t.shape('pizza.gif')
+t.stamp()
+t.shape('pizza.gif')
+
+
+round = input("press Enter to Continue: ")
+t.clear()
+screen.bgcolor('tan')
+t.penup()
+t.goto(0,0)
+t.pendown()
+screen.addshape('pizza.gif')
+t.shape('pizza.gif')
+t.stamp()
+t.shape('pizza.gif')
+
+
+round = input("press Enter to Continue: ")
+t.clear()
+screen.bgcolor('tan')
+t.penup()
+t.goto(0,0)
+t.pendown()
+screen.addshape('pizza.gif')
+t.shape('pizza.gif')
+t.stamp()
+t.shape('pizza.gif')
+
+
+
+round = input("press Enter to Continue: ")
+t.clear()
+screen.bgcolor('tan')
+t.penup()
+t.goto(0,0)
+t.pendown()
+screen.addshape('pizza.gif')
+t.shape('pizza.gif')
+t.stamp()
+t.shape('pizza.gif')
+
+
+
+
+
+turtle.done()
